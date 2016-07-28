9de2d692d62b82268a9c08564ab3ef72955ffb16
James McManus <McBits@users.noreply.github.com>
2016-07-27 23:57:08 -0500

    Initial project

---
+++ src/android/src/com/xmorse/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.xmorse.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.xmorse.game.XMorse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new XMorse(), config);
+	}
+}
---
+++ src/desktop/src/com/xmorse/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xmorse.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xmorse.game.XMorse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XMorse(), config);
+	}
+}
