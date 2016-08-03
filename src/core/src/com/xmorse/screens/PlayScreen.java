638f21debde26bd2407fcd6f28a2f43158f58d0a
James McManus <McBits@users.noreply.github.com>
2016-08-03 04:04:43 -0500

    Add button, hud, actor

--- src/core/src/com/xmorse/screens/PlayScreen.java
+++ src/core/src/com/xmorse/screens/PlayScreen.java
@@ -2,18 +2,31 @@ package com.xmorse.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.xmorse.XMorse;
+import com.xmorse.scenes.Hud;
+import com.xmorse.util.Static;
 
 public class PlayScreen extends ScreenAdapter
 {
 	private final XMorse _game;
 	private boolean _alive = true;
+	private Camera _camera;
+	private Viewport _viewport;
+	private Hud _hud;
 
 	public PlayScreen(XMorse game)
 	{
 		_game = game;
+		_camera = new OrthographicCamera();
+		_viewport = new FitViewport(Static.BaseWidth, Static.BaseHeight, _camera);
+		_hud = new Hud(game.batch);
+		_camera.position.set(_viewport.getWorldWidth() / 2, _viewport.getWorldHeight() / 2, 0);
 	}
 
 	@Override
@@ -21,16 +34,32 @@ public class PlayScreen extends ScreenAdapter
 	{
 	}
 
+	public void handleInput(float dt)
+	{
+		if (Gdx.input.isTouched())
+			_camera.position.x += 10 * dt;
+	}
+
+	public void update(float dt)
+	{
+		handleInput(dt);
+		_camera.update();
+	}
+
 	@Override
-	public void render(float delta)
+	public void render(float dt)
 	{
 		if (!_alive) {
 			_game.setScreen(new GameOverScreen(_game));
 		}
 
-		_game.batch.begin();
+		update(dt);
 
-		_game.batch.end();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		_game.batch.setProjectionMatrix(_hud.stage.getCamera().combined);
+		_hud.stage.draw();
 
 		//_alive = false;
 	}
