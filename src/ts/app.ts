9db4b88d8bdd8aecef49735ca477a429c9724f31
James McManus <jamesmc404@gmail.com>
2016-12-30 08:20:58 -0600

    Add options for speed, volume, pitch

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -47,89 +47,133 @@ const charPatterns: { [char: string]: string } = {
     ",": "--..--",
 }
 
-let frequency = 700;
+const allCharacters = Object.keys(charPatterns);
+
+let pitch = 700;
 let volume = 0.25;
-let speed = 75;
-let ramp = 0.015;
+let wpm = 18;
+let timeUnit = () => 1.2 / wpm * 1000;
+let ramp = 0.012;
+let playing = false;
+let paused = false;
+let currentLetter: string = null;
+let currentPattern: string = null;
 
-const audioCtx: AudioContext = new (AudioContext || window["webkitAudioContext"])();
+// Events
+const patternCompleteEvent = new Event("patterncomplete");
 
-const oscillator = audioCtx.createOscillator();
-oscillator.frequency.value = frequency;
-oscillator.start(0);
-
-const gainNode = audioCtx.createGain();
-gainNode.gain.value = 0;
-
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-
+// Page elements
 const startButton = <HTMLButtonElement>document.querySelector(".btn-start");
 const pauseButton = <HTMLButtonElement>document.querySelector(".btn-pause");
 const stopButton = <HTMLButtonElement>document.querySelector(".btn-stop");
+const letterElement = document.querySelector(".letter");
+const wpmSlider = <HTMLInputElement>document.getElementById("charWPM");
+const volumeSlider = <HTMLInputElement>document.getElementById("volume");
+const pitchSlider = <HTMLInputElement>document.getElementById("pitch");
 
-const patternCompleteEvent = new Event("patterncomplete");
+// Audio parts
+const audioCtx: AudioContext = new (AudioContext || window["webkitAudioContext"])();
+const oscillator = audioCtx.createOscillator();
+const gainNode = audioCtx.createGain();
 
-let currentLetter: string = "";
-let currentPattern: string = "";
+// Wire up audio parts
+oscillator.frequency.value = pitch;
+oscillator.start(0);
+gainNode.gain.value = 0;
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
 
 function random(first, second) {
     return Math.floor(Math.random() * (second - first)) + first + 1;
+}
+
+function randomCharacter() {
+    return allCharacters[Math.floor(Math.random() * allCharacters.length)];
 }
 
 function setVolume(value: number) {
     gainNode.gain.setTargetAtTime(value, audioCtx.currentTime, ramp);
 }
 
-function playPattern(pattern: string) {
+function setButtonStates() {
+    startButton.disabled = playing && !paused;
+    pauseButton.disabled = paused || !playing;
+    stopButton.disabled = !playing;
+}
+
+function playPattern(pattern: string): void {
+    letterElement.innerHTML = pattern;
+
     function playBlip(index: number) {
         const blip = pattern.charAt(index++);
 
-        if (blip === " ") {
-            setVolume(0)
-            setTimeout(() => playBlip(index), speed * 5);
-        }
-        else {
-            setVolume(volume);
+        setVolume(volume);
 
-            let time: number;
-            if (blip === ".")
-                time = speed;
-            else if (blip === "-")
-                time = speed * 3.5;
+        let time: number;
+        if (blip === ".")
+            time = timeUnit();
+        else if (blip === "-")
+            time = timeUnit() * 3;
 
-            setTimeout(() => setVolume(0), time);
+        setTimeout(() => setVolume(0), time);
 
-            if (index < pattern.length)
-                setTimeout(() => playBlip(index), time + speed);
-            else
-                setTimeout(() => document.dispatchEvent(patternCompleteEvent), time + speed);
-        }
+        if (index < pattern.length)
+            setTimeout(() => playBlip(index), time + timeUnit());
+        else
+            setTimeout(() => document.dispatchEvent(patternCompleteEvent), time + timeUnit());
     }
 
     playBlip(0)
 }
 
-function playSequence(chars: string) {
-    function playChar(index: number) {
-        const char = chars.charAt(index++);
-        const pattern = charPatterns[char];
-        playPattern(pattern);
-    }
-}
-
 function nextPattern() {
-    const letters = Object.keys(charPatterns);
-    currentLetter = letters[Math.floor(Math.random() * letters.length)];
+    currentLetter = randomCharacter();
     currentPattern = charPatterns[currentLetter];
 }
 
-document.addEventListener("patterncomplete", (evt: any) => {
-    const letterElement = document.querySelector(".letter");
-    letterElement.innerHTML = currentLetter;
-});
+function updateVolume() {
+    volume = parseFloat(volumeSlider.value) / 100.0;
+    (<HTMLInputElement>document.querySelector("#volumeText")).value = Math.floor(volume * 200).toString();
+}
 
-startButton.addEventListener("click", () => {
+function updateWPM(evt: Event) {
+    wpm = parseInt(wpmSlider.value);
+    (<HTMLInputElement>document.querySelector("#charWPMText")).value = wpm.toString();
+}
+
+function updatePitch(evt: Event) {
+    pitch = parseInt(pitchSlider.value);
+    oscillator.frequency.value = pitch;
+    (<HTMLInputElement>document.querySelector("#pitchText")).value = pitch.toString();
+}
+
+function startPlaying() {
+    playing = true;
+    paused = false;
+    setButtonStates();
     nextPattern();
     playPattern(currentPattern);
-});
+}
+
+function stopPlaying() {
+    playing = false;
+    paused = false;
+    setButtonStates();
+}
+
+function patternComplete() {
+    letterElement.innerHTML = currentLetter;
+    if (playing && !paused) {
+        nextPattern();
+        setTimeout(function() {
+            playPattern(currentPattern);
+        }, timeUnit() * 25);
+    }
+}
+
+wpmSlider.addEventListener("input", updateWPM);
+volumeSlider.addEventListener("input", updateVolume);
+pitchSlider.addEventListener("input", updatePitch);
+document.addEventListener("patterncomplete", patternComplete);
+startButton.addEventListener("click", startPlaying);
+stopButton.addEventListener("click", stopPlaying);
