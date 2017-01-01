1ae8bcd43d3ec64c19b5d4dbf23b1ebc5815876e
James McManus <jamesmc404@gmail.com>
2016-12-31 20:21:40 -0600

    Add voices

--- js/app.js
+++ js/app.js
@@ -142,10 +142,13 @@ var charPatterns = {
     "!": "-.-.--",
     ",": "--..--",
 };
+var audioSources = {};
 var allCharacters = Object.keys(charPatterns);
 var pitch = 700;
-var volume = 0.25;
+var mainVolume = 0.5;
+var oscillatorVolume = 0.9;
 var wpm = 18;
+var charSpacing = 25;
 var timeUnit = function () { return 1.2 / wpm * 1000; };
 var ramp = 0.012;
 var playing = false;
@@ -154,32 +157,52 @@ var currentLetter = null;
 var currentPattern = null;
 // Events
 var patternCompleteEvent = new Event("patterncomplete");
+var audioLoadedEvent = new Event("audioloaded");
 // Page elements
 var startButton = document.querySelector(".btn-start");
 var pauseButton = document.querySelector(".btn-pause");
 var stopButton = document.querySelector(".btn-stop");
 var letterElement = document.querySelector(".letter");
-var wpmSlider = document.getElementById("charWPM");
+// Settings inputs
 var volumeSlider = document.getElementById("volume");
+var charWPMSlider = document.getElementById("charWPM");
 var pitchSlider = document.getElementById("pitch");
+var charSpacingSlider = document.getElementById("charSpacing");
+var voiceEnabledCheckbox = document.getElementById("voiceEnabled");
+// Settings text labels
+var volumeText = document.querySelector(".volumeText");
+var charWPMText = document.querySelector(".charWPMText");
+var pitchText = document.querySelector(".pitchText");
+var charSpacingText = document.querySelector(".charSpacingText");
 // Audio parts
 var audioCtx = new (AudioContext || window["webkitAudioContext"])();
 var oscillator = audioCtx.createOscillator();
-var gainNode = audioCtx.createGain();
+var oscillatorGain = audioCtx.createGain();
+var voiceGain = audioCtx.createGain();
+var masterGain = audioCtx.createGain();
 // Wire up audio parts
 oscillator.frequency.value = pitch;
+oscillatorGain.gain.value = 0;
+oscillator.connect(oscillatorGain);
+oscillatorGain.connect(masterGain);
+voiceGain.gain.value = 0.6;
+voiceGain.connect(masterGain);
+masterGain.gain.value = mainVolume;
+masterGain.connect(audioCtx.destination);
 oscillator.start(0);
-gainNode.gain.value = 0;
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
 function random(first, second) {
     return Math.floor(Math.random() * (second - first)) + first + 1;
 }
 function randomCharacter() {
     return allCharacters[Math.floor(Math.random() * allCharacters.length)];
 }
-function setVolume(value) {
-    gainNode.gain.setTargetAtTime(value, audioCtx.currentTime, ramp);
+function setOscillatorVolume(value) {
+    // NOTE: The +0.05 is only there to (mostly) eliminate clicking due to a bug in Firefox.
+    // It may not be needed in the future.
+    oscillatorGain.gain.setTargetAtTime(value, audioCtx.currentTime + 0.05, 0.01);
+}
+function setMainVolume(value) {
+    masterGain.gain.setTargetAtTime(value, audioCtx.currentTime, 0.01);
 }
 function setButtonStates() {
     startButton.disabled = playing && !paused;
@@ -190,13 +213,13 @@ function playPattern(pattern) {
     letterElement.innerHTML = pattern;
     function playBlip(index) {
         var blip = pattern.charAt(index++);
-        setVolume(volume);
+        setOscillatorVolume(oscillatorVolume);
         var time;
         if (blip === ".")
             time = timeUnit();
         else if (blip === "-")
             time = timeUnit() * 3;
-        setTimeout(function () { return setVolume(0); }, time);
+        setTimeout(function () { return setOscillatorVolume(0); }, time);
         if (index < pattern.length)
             setTimeout(function () { return playBlip(index); }, time + timeUnit());
         else
@@ -209,17 +232,22 @@ function nextPattern() {
     currentPattern = charPatterns[currentLetter];
 }
 function updateVolume() {
-    volume = parseFloat(volumeSlider.value) / 100.0;
-    document.querySelector("#volumeText").value = Math.floor(volume * 200).toString();
+    mainVolume = parseFloat(volumeSlider.value);
+    setMainVolume(mainVolume);
+    volumeText.value = Math.floor(mainVolume * 100).toString();
 }
 function updateWPM(evt) {
-    wpm = parseInt(wpmSlider.value);
-    document.querySelector("#charWPMText").value = wpm.toString();
+    wpm = parseInt(charWPMSlider.value);
+    charWPMText.value = wpm.toString();
 }
 function updatePitch(evt) {
     pitch = parseInt(pitchSlider.value);
     oscillator.frequency.value = pitch;
-    document.querySelector("#pitchText").value = pitch.toString();
+    pitchText.value = pitch.toString();
+}
+function updateCharSpacing(evt) {
+    charSpacing = parseInt(charSpacingSlider.value);
+    charSpacingText.value = charSpacing.toString();
 }
 function startPlaying() {
     playing = true;
@@ -236,16 +264,79 @@ function stopPlaying() {
 function patternComplete() {
     letterElement.innerHTML = currentLetter;
     if (playing && !paused) {
-        nextPattern();
-        setTimeout(function () {
-            playPattern(currentPattern);
-        }, timeUnit() * 25);
+        if (voiceEnabledCheckbox.checked)
+            loadAudio(currentLetter);
+        else {
+            nextPattern();
+            setTimeout(function () {
+                playPattern(currentPattern);
+            }, timeUnit() * charSpacing);
+        }
     }
 }
-wpmSlider.addEventListener("input", updateWPM);
+var charAudioBuffer;
+function getCharName(char) {
+    switch (char) {
+        case "!":
+            return "EXCLAMATION";
+        case "?":
+            return "QUESTION";
+        case ".":
+            return "PERIOD";
+        case ",":
+            return "COMMA";
+        default:
+            return char;
+    }
+}
+function loadAudio(char) {
+    char = getCharName(char);
+    if (typeof audioSources[char] !== "undefined") {
+        document.dispatchEvent(audioLoadedEvent);
+        return audioSources[char];
+    }
+    else {
+        var filename = char.replace(/\W/g, "") + ".mp3";
+        var request_1 = new XMLHttpRequest();
+        request_1.open("GET", "/audio/" + filename, true);
+        request_1.responseType = "arraybuffer";
+        request_1.addEventListener("load", function () {
+            var response = request_1.response;
+            audioCtx.decodeAudioData(response, function (buffer) {
+                var audioSource = audioCtx.createBufferSource();
+                audioSources[char] = buffer;
+                document.dispatchEvent(audioLoadedEvent);
+            }, function (err) { return console.log("Error loading audio source: ", err); });
+        });
+        request_1.send();
+    }
+}
+function playAudio() {
+    setTimeout(function () {
+        var char = getCharName(currentLetter);
+        var buffer = audioSources[char];
+        if (typeof buffer !== "undefined") {
+            var audioSource = audioCtx.createBufferSource();
+            audioSource.addEventListener("ended", function (evt) {
+                nextPattern();
+                setTimeout(function () {
+                    playPattern(currentPattern);
+                }, timeUnit() * charSpacing);
+            });
+            audioSource.buffer = buffer;
+            audioSource.connect(voiceGain);
+            audioSource.start(0);
+        }
+    }, timeUnit() * charSpacing);
+}
+// Settings events
 volumeSlider.addEventListener("input", updateVolume);
+charWPMSlider.addEventListener("input", updateWPM);
+charSpacingSlider.addEventListener("input", updateCharSpacing);
 pitchSlider.addEventListener("input", updatePitch);
+// Playback events
 document.addEventListener("patterncomplete", patternComplete);
+document.addEventListener("audioloaded", playAudio);
 startButton.addEventListener("click", startPlaying);
 stopButton.addEventListener("click", stopPlaying);
 
