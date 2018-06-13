7eddd6398764638cab0dca71f7af580c7af9cb9a
James McManus <mcbits@users.noreply.github.com>
2018-06-13 11:40:57 -0500

    Extract consts from ToneGenrator

--- src/ts/tonegenerator.ts
+++ src/ts/tonegenerator.ts
@@ -1,11 +1,12 @@
 import { AudioCtx } from "./audiocontext";
 import * as Morse from "./morsetable";
 
+const ditUnits = 1;
+const dahUnits = 3;
+const sampleRate = 48000;
+
 export class ToneGenerator
 {
-	private readonly ditUnits = 1;
-	private readonly dahUnits = 3;
-	private readonly sampleRate = 48000;
 	private ramp = 0.01;
 	private frequency = 650;
 	private charSpacing: number = 2;
@@ -23,7 +24,7 @@ export class ToneGenerator
 
 		for (let i = 0; i < pattern.length; ++i)
 		{
-			total += (pattern[i] === "." ? this.ditUnits : this.dahUnits) + this.ditUnits;
+			total += (pattern[i] === "." ? ditUnits : dahUnits) + ditUnits;
 		}
 
 		return total;
@@ -31,10 +32,10 @@ export class ToneGenerator
 
 	private addToneToBuffer(units: number, samplesPerUnit: number, data: Float32Array, initialSample: number): void
 	{
-		const omega = 2 * Math.PI * this.frequency / this.sampleRate;
+		const omega = 2 * Math.PI * this.frequency / sampleRate;
 
 		// Number of audio samples in the ramp up/down periods.
-		const rampSamples = Math.min(Math.ceil(this.ramp * this.sampleRate), samplesPerUnit);
+		const rampSamples = Math.min(Math.ceil(this.ramp * sampleRate), samplesPerUnit);
 
 		// Number of samples containing audio. Ramp-up is included in the "on" time, but ramp-down is appended.
 		const soundSamples = Math.ceil(samplesPerUnit * units);
@@ -66,10 +67,10 @@ export class ToneGenerator
 		const units = this.countUnitsInPattern(pattern);
 
 		// Number of audio samples per Morse code unit of time.
-		const samplesPerUnit = Math.ceil(this.unitTime * this.sampleRate);
+		const samplesPerUnit = Math.ceil(this.unitTime * sampleRate);
 
 		// Number of audio samples in the ramp up/down periods.
-		const rampSamples = Math.min(Math.ceil(this.ramp * this.sampleRate), samplesPerUnit);
+		const rampSamples = Math.min(Math.ceil(this.ramp * sampleRate), samplesPerUnit);
 
 		// Number of samples containing audio. Ramp-up is included in the "on" time, but ramp-down is appended.
 		const soundSamples = Math.ceil(samplesPerUnit * units);
@@ -77,7 +78,7 @@ export class ToneGenerator
 		// Total samples in the clip. This is the "on" time + ramp-down time + any remaining silence.
 		const totalSamples = Math.ceil(samplesPerUnit * units + (samplesPerUnit * this.charSpacing));
 
-		const buffer = AudioCtx.createBuffer(1, totalSamples, this.sampleRate);
+		const buffer = AudioCtx.createBuffer(1, totalSamples, sampleRate);
 
 		const data = buffer.getChannelData(0);
 
@@ -88,12 +89,12 @@ export class ToneGenerator
 			switch (pattern[i])
 			{
 				case ".":
-					this.addToneToBuffer(this.ditUnits, samplesPerUnit, data, pos);
-					pos += samplesPerUnit * (this.ditUnits + this.ditUnits);
+					this.addToneToBuffer(ditUnits, samplesPerUnit, data, pos);
+					pos += samplesPerUnit * (ditUnits + ditUnits);
 					break;
 				case "-":
-					this.addToneToBuffer(this.dahUnits, samplesPerUnit, data, pos);
-					pos += samplesPerUnit * (this.dahUnits + this.ditUnits);
+					this.addToneToBuffer(dahUnits, samplesPerUnit, data, pos);
+					pos += samplesPerUnit * (dahUnits + ditUnits);
 					break;
 			}
 		}
@@ -107,10 +108,10 @@ export class ToneGenerator
 
 		const emptyChar = Morse.GetCharacter("");
 		const spaceChar = Morse.GetCharacter(" ");
-		const charSpaceTime = this.unitTime * this.sampleRate * this.charSpacing;
+		const charSpaceTime = this.unitTime * sampleRate * this.charSpacing;
 
-		emptyChar.toneAudioBuffer = AudioCtx.createBuffer(1, charSpaceTime, this.sampleRate);
-		spaceChar.toneAudioBuffer = AudioCtx.createBuffer(1, charSpaceTime * 3, this.sampleRate);
+		emptyChar.toneAudioBuffer = AudioCtx.createBuffer(1, charSpaceTime, sampleRate);
+		spaceChar.toneAudioBuffer = AudioCtx.createBuffer(1, charSpaceTime * 3, sampleRate);
 	}
 
 	SetFrequency(value: number)
